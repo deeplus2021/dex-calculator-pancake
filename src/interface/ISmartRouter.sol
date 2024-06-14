f4a6189af9cb96e33754da7ce5765ff16f04c136
biginfo2012 <biginfo2012@gmail.com>
2024-06-14 19:38:20 +0200

    updated pancake buyer contract

---
+++ src/interface/ISmartRouter.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+interface ISmartRouter {
+    struct ExactOutputSingleParams {
+        address tokenIn;
+        address tokenOut;
+        uint24 fee;
+        address recipient;
+        uint256 deadline;
+        uint256 amountOut;
+        uint256 amountInMaximum;
+        uint160 sqrtPriceLimitX96;
+    }
+
+    /// @notice Swaps as little as possible of one token for `amountOut` of another token
+    /// @param params The parameters necessary for the swap, encoded as `ExactOutputSingleParams` in calldata
+    /// @return amountIn The amount of the input token
+    function exactOutputSingle(ExactOutputSingleParams calldata params) external payable returns (uint256 amountIn);
+
+    /// @notice Swaps as little as possible of one token for an exact amount of another token
+    /// @param amountOut The amount of token to swap for
+    /// @param amountInMax The maximum amount of input that the caller will pay
+    /// @param path The ordered list of tokens to swap through
+    /// @param to The recipient address
+    /// @return amountIn The amount of token to pay
+    function swapTokensForExactTokens(
+        uint256 amountOut,
+        uint256 amountInMax,
+        address[] calldata path,
+        address to
+    ) external payable returns (uint256 amountIn);
+}
